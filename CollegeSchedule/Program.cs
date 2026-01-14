192d76eeb619618d3ec04c7f6b4ed81818d9bfea
Чернявской Леонид <leonid.chernyavsky@example.com>
2026-01-14 16:16:10 +0700

    настроил Program.cs реализовал папки DTO и Services

--- CollegeSchedule/Program.cs
+++ CollegeSchedule/Program.cs
@@ -1,6 +1,18 @@
+using CollegeSchedule.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+DotNetEnv.Env.Load();
+var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+ $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+ $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+ $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+
+$"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+builder.Services.AddDbContext<AppDbContext>(options =>
+ options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,6 +31,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseRouting();
 
 app.MapControllers();
 
